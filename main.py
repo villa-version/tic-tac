0a94f5a8e718cbe79ba5ec0346fb054b85de19b0
villa <volinetsilia@gmail.com>
2021-08-14 22:15:55 +0300

    Added ability to win

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ class Object:
 
 
 class Grid(Object):
-    def __init__(self, x, y, screen):
+    def __init__(self, x, y, screen, occupied):
         Object.__init__(self, x, y, screen)
+        self.occupied = occupied
 
     def draw(self):
         for i in range(2):
@@ -46,8 +47,8 @@ class MainController:
     def __init__(self, screen):
         self.screen = screen
         self.grid = []
-        self.ocupied_blocks = []
         self.spawn_grid()
+        self.copied_grid = self.grid
         self.zeros = []
         self.crosses = []
         self.progress = 'Zero'
@@ -56,10 +57,9 @@ class MainController:
         self.draw_elements()
 
     def draw_elements(self):
-        for block in self.grid:
-            block.draw()
-        for block in self.ocupied_blocks:
-            block.draw()
+        for row in self.grid:
+            for block in row:
+                block.draw()
         for zero in self.zeros:
             zero.draw()
         for cross in self.crosses:
@@ -73,30 +73,47 @@ class MainController:
 
     def spawn_grid(self):
         r = range(CELL_NUMBER)
+        for _ in r:
+            self.grid.append([])
         for y in r:
             for x in r:
-                self.grid.append(Grid(x, y, self.screen))
-
-    def spawn_ocupied_blocks(self, x, y):
-        self.ocupied_blocks.append(Grid(x, y, self.screen))
+                self.grid[y].append(Grid(x, y, self.screen, ""))
 
     def check_press_on_area(self, mx, my):
-        for block in self.grid[:]:
-            x = block.x * CELL_SIZE
-            y = block.y * CELL_SIZE
-            if x < mx < x + CELL_SIZE and y < my < y + CELL_SIZE:
-                self.spawn_ocupied_blocks(block.x, block.y)
-                if self.progress == 'Zero':
-                    self.progress = 'Cross'
-                    self.spawn_zero(block.x, block.y)
-                elif self.progress == 'Cross':
-                    self.progress = 'Zero'
-                    self.spawn_cross(block.x, block.y)
-                self.grid.remove(block)
+        for row in self.grid[:]:
+            for block in row:
+                x = block.x * CELL_SIZE
+                y = block.y * CELL_SIZE
+                if x < mx < x + CELL_SIZE and y < my < y + CELL_SIZE:
+                    if block.occupied != '':
+                        pass
+                    else:
+                        if self.progress == 'Zero':
+                            self.progress = 'Cross'
+                            self.spawn_zero(block.x, block.y)
+                            block.occupied = 'o'
+                        elif self.progress == 'Cross':
+                            self.progress = 'Zero'
+                            self.spawn_cross(block.x, block.y)
+                            block.occupied = 'x'
+
+    def check_win(self):
+        for sign in ('x', 'o'):
+            for col in range(CELL_NUMBER):
+                if self.grid[col][0].occupied == sign and self.grid[col][1].occupied == sign and self.grid[col][2].occupied == sign:
+                    return sign
+            for elem in range(CELL_NUMBER):
+                if self.grid[0][elem].occupied == sign and self.grid[1][elem].occupied == sign and self.grid[2][elem].occupied == sign:
+                    return sign
+            if self.grid[0][0].occupied == sign and self.grid[1][1].occupied == sign and self.grid[2][2].occupied == sign:
+                return sign
+            elif self.grid[2][0].occupied == sign and self.grid[1][1].occupied == sign and self.grid[2][0].occupied == sign:
+                return sign
 
 
 def main():
     screen = pygame.display.set_mode((CELL_SIZE * CELL_NUMBER, CELL_SIZE * CELL_NUMBER))
+    pygame.display.set_caption("Tic-Tac")
 
     main_controller = MainController(screen)
 
@@ -108,6 +125,15 @@ def main():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 m_pos = pygame.mouse.get_pos()
                 main_controller.check_press_on_area(m_pos[0], m_pos[1])
+                result = main_controller.check_win()
+                if result == 'x':
+                    print('CROSS WON')
+                    pygame.quit()
+                    sys.exit()
+                elif result == 'o':
+                    print('ZEROS WON')
+                    pygame.quit()
+                    sys.exit()
 
         screen.fill((255, 255, 255))
         main_controller.update()
